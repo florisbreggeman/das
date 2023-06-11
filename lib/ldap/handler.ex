86472191af20fd4470b957f3562dc1b906aaf8bf
florisbreggeman <floris@sfbtech.nl>
2023-06-11 12:57:52 +0000

    first LDAP handler that can complete one auth session

--- lib/ldap/handler.ex
+++ lib/ldap/handler.ex
@@ -1,6 +1,9 @@
 defmodule LDAP.Handler do
 
+  @dont_include [:admin, :password]
+
   import Ecto.Query
+  require Logger
 
   def start_link(ref, socket, transport, opts) do
     pid = spawn_link(fn -> init(ref, socket, transport, opts) end)
@@ -19,16 +22,17 @@ defmodule LDAP.Handler do
         msg = :ldap_asn.decode(:LDAPMessage, data)
         case msg do
           {:ok, {:LDAPMessage, seq, request, _controls}} ->
+            Logger.debug("LDAP request: " <> inspect(request))
             {responses, new_bind} = handle(request, bind)
-            IO.inspect(responses)
             cond do
               is_list(responses) ->
                 Enum.map(responses, fn response ->
-                  IO.inspect(response)
+                  Logger.debug("LDAP response: " <> inspect(response))
                   {:ok, response} = :ldap_asn.encode(:LDAPMessage, {:LDAPMessage, seq, response, :asn1_NOVALUE})
                   transport.send(socket, response)
                 end)
               responses != nil ->
+                Logger.debug("LDAP response: " <> inspect(responses))
                 {:ok, response} = :ldap_asn.encode(:LDAPMessage, {:LDAPMesssage, seq, responses, :asn1_NOVALUE})
                 transport.send(socket, response)
               true -> nil #response is nil, don't do anything
@@ -47,11 +51,15 @@ defmodule LDAP.Handler do
   end
 
   defp handle({:bindRequest, {:BindRequest, _version, dn, {:simple, password}}}, bind) do
+    #first filter on whether we are trying to authenticate a client application or a user,
+    #Notable difference: applications have an id, whereas users have a username
     client = case dn do
       "id=" <> id -> 
         id = String.split(id, ",") |> Enum.at(0) #we only care about the first RDN
-        IO.inspect(id)
         Clients.verify(id, password)
+      "username=" <> username ->
+        username = String.split(username, ",") |> Enum.at(0)
+        Users.verify(username, password)
       _ -> 
         nil
     end
@@ -68,27 +76,47 @@ defmodule LDAP.Handler do
     {response, bind}
   end
 
-  defp handle({:searchRequest, {:SearchRequest, _domain, _subtree, _deref, size, _time, _typesonly, filters, attributes}}, bind) do
+  defp handle({:searchRequest, {:SearchRequest, _domain, _subtree, _deref, size, _time, _typesonly, filters, _attributes}}, bind) do
+    #TODO: actually use the attribute selection
     if bind == nil do
       response = {:searchResDone, {:LDAPResult, :insufficientAccessRights, "", "You must be authenticated to search", :asn1_NOVALUE}}
       {response, bind}
     else
       query = from Users.User
       query = build_query(filters, query)
+      query = if size > 0 do
+        from query, limit: ^size
+      else
+        query
+      end
       repo = Storage.get()
       users = repo.all(query)
       #Below we see the most efficient way to put a predefined value at the end of a generated list (prepending is very efficient, appending is not)
       responses = [{:searchResDone, {:LDAPResult, :success, "", "", :asn1_NOVALUE}}]
       responses = Enum.reduce(users, responses, fn user, responses ->
-        [{:searchResEntry, {:SearchResEntry, "cn=" <> user.username <> ",dc=das,dc=nl", []}} | responses]
+        [{:searchResEntry, {:SearchResEntry, "username=" <> user.username <> ",dc=das,dc=nl", 
+          Enum.map(Users.User.__schema__(:fields), fn field ->
+            if Enum.member?(@dont_include, field) do
+              nil
+            else
+              value = Map.get(user, field)
+              value = if field == :id do Integer.to_string(value) else value end
+              if value == nil do
+                nil
+              else
+                {:partialAttribute, Atom.to_string(field), [value]}
+              end
+            end
+          end)
+          |> Enum.filter(fn x -> x != nil end) 
+        }} | responses]
       end)
       {responses, bind}
     end
   end
 
   # unknown requests, also handles unbinds
-  defp handle(request, _bind) do
-    IO.inspect(request)
+  defp handle(_request, _bind) do
     {nil, :close}
   end
 
