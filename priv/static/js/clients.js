9d3e77249179c15b9af45fedeb33ea0a73f49085
florisbreggeman <floris@sfbtech.nl>
2023-06-11 12:57:54 +0000

    write CSS for clients.html

--- priv/static/js/clients.js
+++ priv/static/js/clients.js
@@ -1,25 +1,26 @@
 const PATH_CLIENT = "admin/client";
 const PATH_LDAP_AREA = "admin/client_ldap_area";
 
-function add_field(description, id, value, box){
-    let description_element = document.createElement('b');
+function add_field(description, value_name, value, box, item_id, disabled=false){
+    let description_element = document.createElement('label');
+    description_element.for = 'client-'+item_id+'-'+value_name;
+    description_element.id = description_element.for+'-label';
     description_element.appendChild(document.createTextNode(description));
     let field = document.createElement('input');
-    field.classList.add(id)
+    field.id = 'client-'+item_id+'-'+value_name;
+    field.classList.add(value_name)
+    field.classList.add('pure-input');
+    field.classList.add('pure-input-1');
     field.value = value
     field.type = "text"
+    if(disabled){
+        field.readOnly = true;
+    }
 
     box.appendChild(description_element);
     box.appendChild(field)
-    box.appendChild(document.createElement('br'));
 
     return field;
-}
-
-function notify(text){
-    element = document.getElementById('notifications');
-    removeAllChildNodes(element);
-    element.appendChild(document.createTextNode(text));
 }
 
 function edit(clientid){
@@ -37,11 +38,11 @@ function edit(clientid){
     }
 
     apiCall(PATH_CLIENT+"/"+String(clientid), PUT, body, function(res){
-        notify("Client application updated");
+        Jackbox.success("Client application updated");
         load();
     }, function(res, status){
         if(status==409){
-            notify("There was a conflict updating this application: " + String(res));
+            Jackbox.error("There was a conflict updating this application: " + String(res));
         }
         if(status==403){
             window,location.replace('login.html?redirect=clients.html')
@@ -56,30 +57,22 @@ function load(){
 
         res.forEach(function(item){
             element = document.createElement('li');
+            element.classList.add('pure-form');
+            element.classList.add('pure-form-aligned');
             element.id='client-'+String(item.id);
 
-            type_description = document.createElement('b');
-            type_description.appendChild(document.createTextNode('Type: '));
-            element.appendChild(type_description);
-            type_field = document.createElement('span');
-            type_field.appendChild(document.createTextNode(item.type));
-            element.appendChild(type_field);
-            element.appendChild(document.createElement('br'));
-
-            id_description = document.createElement('b');
-            element.appendChild(id_description);
-            id_field = document.createElement('span');
-            id_field.appendChild(document.createTextNode(item.id));
+            add_field('Type', 'type', item.type, element, item.id, true);
             if(item.type == "ldap"){
-                id_field.classList.add("ldapClient");
-                id_description.appendChild(document.createTextNode('Bind DN: '));
-            }else{
-                id_description.appendChild(document.createTextNode('Id: '));
+                let field = add_field('Bind DN', 'id', item.id, element, item.id, true);
+                field.classList.add('ldap_dn');
+                if("ldapArea" in globalThis){
+                    field.value = "id="+field.value+","+globalThis.ldapArea;
+                }
+            }else if(item.type == "oauth"){
+                add_field('Client Id', 'id', item.id, element, item.id, true);
             }
-            element.appendChild(id_field);
-            element.appendChild(document.createElement('br'));
 
-            name_field = add_field("Name: ", 'name', item.name, element);
+            name_field = add_field("Name", 'name', item.name, element, item.id);
             onEnter(name_field, function(){
                 edit(item.id)
             });
@@ -87,20 +80,20 @@ function load(){
             let url_field;
             switch(item.type){
                 case "forward":
-                    url_field = add_field("Domain: ", 'url', item.url, element);
+                    url_field = add_field("Domain", 'url', item.url, element, item.id);
                     break;
                 case "proxy":
-                    url_field = add_field("Domain: ", 'url', item.url, element);
+                    url_field = add_field("Domain", 'url', item.url, element, item.id);
                     break;
                 default:
-                    url_field = add_field("URL: ", 'url', item.url, element);
+                    url_field = add_field("URL", 'url', item.url, element, item.id);
             }
             onEnter(url_field, function(){
                 edit(item.id)
             });
 
             if(item.type === "proxy"){
-                let destination_field = add_field("Proxy to: ", 'destination', item.destination, element);
+                let destination_field = add_field("Proxy destination", 'destination', item.destination, element, item.id);
                 onEnter(destination_field, function(){
                     edit(item.id)
                 });
@@ -113,73 +106,126 @@ function load(){
                 load_callbacks(item.id, callbacks);
             }
 
-            edit_button = document.createElement('button');
+            let button_group = document.createElement('button-group');
+            button_group.role = 'group';
+            button_group.classList.add('pure-button-group');
+
+            let edit_button = document.createElement('button');
             edit_button.appendChild(document.createTextNode('Edit Data'));
-            element.appendChild(edit_button);
+            edit_button.classList.add('pure-button');
+            edit_button.classList.add('pure-button-primary');
+            button_group.appendChild(edit_button);
             edit_button.addEventListener("click", function(){edit(item.id);});
             
-            secret_button = document.createElement('button');
-            secret_button.appendChild(document.createTextNode('View credentials'));
-            secret_button.addEventListener("click", function(){view_credentials(item.id);});
-            element.appendChild(secret_button);
+            if(item.type == 'oauth' || item.type == 'ldap'){
+                let secret_button = document.createElement('button');
+                secret_button.appendChild(document.createTextNode('View credentials'));
+                secret_button.classList.add('pure-button');
+                secret_button.id = 'client-'+item.id+'-secret_button';
+                secret_button.addEventListener("click", function(){view_credentials(item.id);});
+                button_group.appendChild(secret_button);
+            }
 
-            delete_button = document.createElement('button');
+            let delete_button = document.createElement('button');
             delete_button.appendChild(document.createTextNode('Delete'));
+            delete_button.classList.add('pure-button');
+            delete_button.classList.add('button-delete');
             delete_button.addEventListener("click", function(){delete_client(item.id);});
-            element.appendChild(delete_button);
-
+            button_group.appendChild(delete_button);
+    
+            element.appendChild(button_group);
             list.appendChild(element);
         });
     }, function(res, status){
         if(status == 403){
             window.location.replace('login.html?redirect=clients.html');
         }else{
-            notify("Error when loading: " + String(res));
+            Jackbox.error("Error when loading: " + String(res));
         }
     });
 }
 
 function load_ldapArea(){
-    apiCall(PATH_LDAP_AREA, GET, null, function(res){
-        style = document.querySelector('style#js');
-        style.innerHTML = ".ldapClient:before { content: \"id=\"; } .ldapClient:after { content: \"," + res + "\"; }"
-    })
+    if("ldapArea" in globalThis){
+        apply_ldapArea(globalThis.ldapArea)
+    }else{
+        apiCall(PATH_LDAP_AREA, GET, null, function(res){
+            globalThis.ldapArea=res;
+            apply_ldapArea(res);
+        })
+    }
 }
+
+function apply_ldapArea(area){
+    let elements = document.getElementsByClassName('ldap_dn');
+    for(let el of elements){
+        el.value = "id="+el.value+","+area;
+    }
+}
+
 
 function load_callbacks(client_id, element){
     apiCall(PATH_CLIENT+"/"+client_id+"/callbacks", GET, null, function(res){
         removeAllChildNodes(element);
 
         let title = document.createElement('b')
-        title.appendChild(document.createTextNode("Callback URIs:"))
+        title.appendChild(document.createTextNode("Callback URIs"))
         element.appendChild(title)
         element.appendChild(document.createElement('br'))
-        let ul = document.createElement('ul');
+        let ul = document.createElement('div');
+        ul.classList.add('pure-g');
         res.forEach(function(callback){
-            let li = document.createElement('li');
-            li.appendChild(document.createTextNode(callback))
-            li.appendChild(document.createTextNode("\u00A0\u00A0\u00A0\u00A0")); //add some whitespace
+            let div_1 = document.createElement('div');
+            div_1.classList.add('pure-u-2-3');
+            div_1.classList.add('pure-u-md-4-5');
+            let input = document.createElement('input');
+            input.type = 'text';
+            input.classList.add('pure-input');
+            input.classList.add('pure-input-1');
+            input.value = callback;
+            input.readOnly = true;
+            div_1.appendChild(input)
+            let div_2 = document.createElement('div');
+            div_2.classList.add('pure-u-1-3');
+            div_2.classList.add('pure-u-md-1-5');
             let button = document.createElement('button');
-            button.appendChild(document.createTextNode("delete"));
+            button.appendChild(document.createTextNode("Delete"));
+            button.classList.add('pure-button');
+            button.classList.add('button-delete');
+            button.style = 'width: 100%';
             button.addEventListener("click", function(){delete_callback(client_id, callback, li)});
-            li.appendChild(button);
+            div_2.appendChild(button);
 
-            ul.appendChild(li)
+            ul.appendChild(div_1);
+            ul.appendChild(div_2);
         });
         element.appendChild(ul);
 
+        let field_div = document.createElement('div');
+        field_div.classList.add('pure-u-2-3');
+        field_div.classList.add('pure-u-md-4-5');
         let field = document.createElement('input');
         field.type = 'text';
+        field.classList.add('pure-input');
+        field.classList.add('pure-input-1');
         field.placeholder = 'Callback URI';
-        element.appendChild(field);
+        field_div.appendChild(field);
 
+        let button_div = document.createElement('div');
+        button_div.classList.add('pure-u-md-1-5');
         let button = document.createElement('button');
-        button.appendChild(document.createTextNode("Add new Callback URI"));
+        button.appendChild(document.createTextNode("Add"));
+        button.classList.add('pure-button');
+        button.classList.add('pure-button-primary');
+        button.style = 'width: 100%;';
         button.addEventListener("click", function(){add_callback(client_id, field, element);});
-        element.appendChild(button);
+        button_div.appendChild(button);
+
+        element.appendChild(field_div);
+        element.appendChild(button_div);
 
     }, function(status, msg){
-        notify("Failed loading callbacks for " + client_id + ": " + msg)
+        Jackbox.error("Failed loading callbacks for " + client_id + ": " + msg)
     });
 }
 
@@ -188,10 +234,10 @@ function add_callback(client_id, field, element){
         "uri": field.value
     }
     apiCall(PATH_CLIENT+"/"+client_id+"/callbacks", POST, body, function(res){
-        notify("Added Callback URL");
+        Jackbox.success("Added Callback URL");
         load_callbacks(client_id, element);
     }, function(msg, status){
-        notify("Error adding callback URL: " + msg)
+        Jackbox.error("Error adding callback URL: " + msg)
     });
 }
 
@@ -202,7 +248,7 @@ function delete_callback(client_id, uri, element){
     apiCall(PATH_CLIENT+"/"+client_id+"/callbacks", DELETE, body, function(res){
         element.remove()
     }, function(status, msg){
-        notify("Failed removing callback " + uri + " for " + client_id + ": " + msg)
+        Jackbox.error("Failed removing callback " + uri + " for " + client_id + ": " + msg)
     });
 }
 
@@ -227,74 +273,98 @@ function create_client(){
         body.destination = destination_field.value
     }
     apiCall(PATH_CLIENT, POST, body, function(res){
-        notify("Created client");
+        Jackbox.success("Created client");
         load();
     }, function(res, status){
         if(status == 403){
             window.location.replace("login.html?redirect=clients.html");
         }else{
-            notify("Error creating client: " + String(res));
+            Jackbox.error("Error creating client: " + String(res));
         }
     });
 }
 
 function delete_client(id){
     apiCall(PATH_CLIENT+"/"+String(id), DELETE, null, function(res){
-        notify("Deleted client");
+        Jackbox.success("Deleted client");
         load();
     }, function(res, status){
         if(status===403){
             window.location.replace("login.html?redirect=clients.html");
         }else{
-            notify("Error deleting user: " + String(res))
+            Jackbox.error("Error deleting user: " + String(res))
         }
     });
 }
 
 function view_credentials(id){
     apiCall(PATH_CLIENT+"/"+String(id)+"/credentials", GET, null, function(res){
-        notify("Secret for this application: " + res.secret)
+        let client_name_field = document.getElementById('client-'+id+'-name-label');
+        let client_form = client_name_field.parentElement;
+        let secret_label = document.createElement('label');
+        secret_label.for = 'client-'+id+'-secret';
+        secret_label.appendChild(document.createTextNode('Secret'));
+        client_form.insertBefore(secret_label, client_name_field);
+        let secret_field = document.createElement('input');
+        secret_field.id = 'client-'+id+'-secret';
+        secret_field.classList.add('pure-input');
+        secret_field.classList.add('pure-input-1');
+        secret_field.value = res.secret;
+        secret_field.readOnly = true;
+        client_form.insertBefore(secret_field, client_name_field);
+
+        let secret_button = document.getElementById('client-'+id+'-secret_button');
+        secret_button.classList.add('pure-button-disabled');
+        
     }, function(res, status){
         if(status===403){
             window.location.replace("login.html?redirect=clients.html")
         }else{
-            notify("Error retrieving credentials: " + String(res))
+            Jackbox.error("Error retrieving credentials: " + String(res))
         }
     });
 }
 
 function set_url_placeholder(){
     let type_field = document.getElementById("type");
-    let url_field = document.getElementById("url");
+    let url_label = document.getElementById("url_label");
 
     let value = type_field.value
     switch(value){
         case "forward":
-            url_field.placeholder = "Service Domain";
+            removeAllChildNodes(url_label);
+            url_label.appendChild(document.createTextNode("Service Domain"));
             break;
         case "proxy":
-            url_field.placeholder = "Service Domain";
+            removeAllChildNodes(url_label);
+            url_label.appendChild(document.createTextNode("Service Domain"));
             break;
         default:
-            url_field.placeholder = "URL";
+            removeAllChildNodes(url_label);
+            url_label.appendChild(document.createTextNode("URL"));
     }
 
     let form = document.getElementById("form");
     if(value === "proxy"){
+        let destination_label = document.createElement("label");
+        destination_label.for = "destination";
+        destination_label.appendChild(document.createTextNode("Address to proxy to, in full"));
+        destination_label.id = "destination_label";
         let destination_field = document.createElement("input");
         destination_field.type = "text";
         destination_field.id = "destination";
-        destination_field.placeholder = "Address to proxy to (in full)";
-        br = document.createElement("br");
-        br.id = "destination_br";
-        form.insertBefore(destination_field, type_field);
-        form.insertBefore(br, type_field);
+        destination_field.classList.add('pure-input');
+        destination_field.classList.add('pure-input-1');
+
+        let type_label = document.getElementById("type_label");
+        form.insertBefore(destination_label, type_label);
+        form.insertBefore(destination_field, type_label);
     }else{
         let destination_field = document.getElementById("destination");
         if(destination_field != null){
             destination_field.remove();
-            let destination_br = document.getElementById("destination_br");
-            destination_br.remove();
+            let destination_label = document.getElementById("destination_label");
+            destination_label.remove();
         }
     }
 }
@@ -302,6 +372,8 @@ function set_url_placeholder(){
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
+    Jackbox.init()
+
     name_field = document.getElementById("name");
     onEnter(name_field, create_client);
 
