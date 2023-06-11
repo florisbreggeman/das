73b32c29369267fc9650388af2f28170010b6fe1
florisbreggeman <floris@sfbtech.nl>
2023-06-11 12:57:51 +0000

    update schema to actually have constraints

--- priv/postgres/migrations/20230209141622_create_user.exs
+++ priv/postgres/migrations/20230209141622_create_user.exs
@@ -1,14 +1,21 @@
 defmodule Storage.MySQL.Migrations.CreateUser do
   use Ecto.Migration
 
-  def change do
+  def up do
     create table("user") do
-      add :username, :string
-      add :given_names, :string
-      add :family_name, :string
-      add :email, :string
-      add :admin, :boolean
-      add :password, :string
+      add :username, :string, null: false
+      add :given_names, :string, default: "", null: false
+      add :family_name, :string, default: "", null: false
+      add :email, :string, null: false
+      add :admin, :boolean, default: false, null: false
+      add :password, :string, null: false
     end
+    unique_index("users", [:username])
+    unique_index("users", [:email])
   end
+
+  def down do
+    drop table("user")
+  end
+
 end
